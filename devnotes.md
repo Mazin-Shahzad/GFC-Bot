aeff053810d0b76265339b1b93c8eabd818da7fc
Mazin-Shahzad <141409017+Mazin-Shahzad@users.noreply.github.com>
2023-08-09 01:13:52 +0000

    v2.3
    - created `fighters.json`
    - dumped data from `fighters `dictionary to `fighters.json`
    - implemented functionality to access fighters' data from `fighters.json` and integrated it with `!fighterdetails`

--- devnotes.md
+++ devnotes.md
@@ -1,13 +1,14 @@
-# DEVELOPMENT NOTES: v2.2.2
+# DEVELOPMENT NOTES: v2.3
 
 ## Update Notes:
-- updated next_event_date to refelct correct GFC 007 date
+- created `fighters.json`
+- dumped data from `fighters `dictionary to `fighters.json`
+- implemented functionality to access fighters' data from `fighters.json` and integrated it with `!fighterdetails`
 
 ## Upcoming:
 - Major new release version 3.0
 
-## To Do: v3.0
+## To Do: by v3.0
 -  command revamps
--  .json file fighters database
 -  data input
 -  and more!
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#v2.2.2
+#v2.3
 
 import discord
 from discord.ext import commands
@@ -6,6 +6,7 @@ import os
 from datetime import datetime as dt
 import pytz
 from keep_alive import keep_alive
+import json
 
 last_event_no = 6
 next_event_no = last_event_no + 1
@@ -32,126 +33,10 @@ days_until_cup = abs(
 ).days
 
 #Fighters database
-fighters = {
-    "Shaheer": {
-        "weight_class": "LIGHT-HEAVYWEIGHT",
-        "weight": "75 kg",
-        "record": "2-0-0",
-        "last_fight": "GFC 005",
-        "last_fight_result": "Shaheer beat Mazhar 3 rounds to 0",
-        "pound_for_pound_rank": 1,
-        "total_fouls": 1,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 6,
-        "total_rounds_lost": 0
-    },
-    "Mowahid": {
-        "weight_class": "LIGHTWEIGHT",
-        "weight": "57 kg",
-        "record": "1-0-0",
-        "last_fight": "GFC 001",
-        "last_fight_result": "Mowahid beat Mazhar 3 rounds to 2",
-        "pound_for_pound_rank": 3,
-        "total_fouls": 0,
-        "current_champion": False,
-        "former_champion": True,
-        "total_rounds_won": 3,
-        "total_rounds_lost": 2
-    },
-    "Mazin": {
-        "weight_class": "LIGHTWEIGHT",
-        "weight": "52 kg",
-        "record": "1-0-0",
-        "last_fight": "GFC 002",
-        "last_fight_result": "Mazin beat Ubaid 2 rounds to 1",
-        "pound_for_pound_rank": 2,
-        "total_fouls": 1,
-        "current_champion": True,
-        "former_champion": False,
-        "total_rounds_won": 2,
-        "total_rounds_lost": 1
-    },
-    "Ayaan": {
-        "weight_class": "LIGHTWEIGHT",
-        "weight": "52 kg",
-        "record": "1-1-0",
-        "last_fight": "GFC 006",
-        "last_fight_result":
-        "Ayaan lost to Mazhar 1 round to 2",
-        "pound_for_pound_rank": 6,
-        "total_fouls": 3,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 2,
-        "total_rounds_lost": 2
-    },
-    "Ubaid": {
-        "weight_class": "MIDDLEWEIGHT",
-        "weight": "65 kg",
-        "record": "1-2-0",
-        "last_fight": "GFC 004",
-        "last_fight_result": "Ubaid beat Mazhar 2 rounds to 1",
-        "pound_for_pound_rank": 4,
-        "total_fouls": 4,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 3,
-        "total_rounds_lost": 6
-    },
-    "Daniyal": {
-        "weight_class": "HEAVYWEIGHT",
-        "weight": "95 kg",
-        "record": "0-0-0",
-        "last_fight": None,
-        "last_fight_result": None,
-        "pound_for_pound_rank": None,
-        "total_fouls": 0,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 0,
-        "total_rounds_lost": 0
-    },
-    "Musa": {
-        "weight_class": "LIGHT-HEAVYWEIGHT",
-        "weight": "75 kg",
-        "record": "0-0-0",
-        "last_fight": None,
-        "last_fight_result": None,
-        "pound_for_pound_rank": None,
-        "total_fouls": 0,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 0,
-        "total_rounds_lost": 0
-    },
-    "Sharafat": {
-        "weight_class": "LIGHTWEIGHT",
-        "weight": "50 kg",
-        "record": "0-0-0",
-        "last_fight": None,
-        "last_fight_result": None,
-        "pound_for_pound_rank": None,
-        "total_fouls": 0,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 0,
-        "total_rounds_lost": 0
-    },
-    "Mazhar": {
-        "weight_class": "LIGHT-HEAVYWEIGHT",
-        "weight": "75 kg",
-        "record": "1-4-0",
-        "last_fight": "GFC 006",
-        "last_fight_result": "Mazhar Beat Ayaan 2 rounds to 1",
-        "pound_for_pound_rank": 5,
-        "total_fouls": 9,
-        "current_champion": False,
-        "former_champion": False,
-        "total_rounds_won": 5,
-        "total_rounds_lost": 10
-    }
-}
+def load_fighters_data():
+    with open("fighters.json", "r") as f:
+        fighters = json.load(f)
+    return fighters
 
 # Bot token.
 TOKEN = os.getenv("TOKEN")
@@ -191,6 +76,7 @@ async def response(ctx):
 
 @bot.command(name="fighterdetails")
 async def response(ctx, name, *selected_details):
+  fighters = load_fighters_data()
   if not name:
     await ctx.send("Error: Please enter a fighter's name.")
     return
@@ -231,7 +117,6 @@ async def send_fighter_details(ctx, name, details, selected_details=None):
       formatted_key = key.replace('_', ' ').title()
       msg += f"{formatted_key}: {value}\n"
   await ctx.send(msg)
-
 
 #Devnotes command:
 @bot.command(name="devnotes")
